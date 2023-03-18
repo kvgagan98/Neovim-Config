dde0fa3a7551e9fafa116732cef423332161ab27
kvgagan98 <kvgagan98@gmail.com>
2023-03-18 13:07:57 -0700

    Fixed some indentation in options.lua

--- lua/gagan/options.lua
+++ lua/gagan/options.lua
@@ -1,7 +1,7 @@
-vim.opt.number                  = true			--Enable Line Numbers
-vim.opt.relativenumber          = true			--Enable Relative line #'s
-vim.opt.tabstop                 = 4				--4 spaces for a tab (VS Code setting)
-vim.opt.expandtab               = true 			--Expand tabs to spaces
-vim.opt.cursorline              = true			--Highlight current line
+vim.opt.number                  = true          --Enable Line Numbers
+vim.opt.relativenumber          = true          --Enable Relative line #'s
+vim.opt.tabstop                 = 4             --4 spaces for a tab (VS Code setting)
+vim.opt.expandtab               = true          --Expand tabs to spaces
+vim.opt.cursorline              = true          --Highlight current line
 vim.opt.scrolloff               = 8             --# of Lines below current line
 vim.opt.ignorecase              = true
