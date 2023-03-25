ab65032aad4c67d9a01756b3dabec671d03712af
kvgagan98 <kvgagan98@gmail.com>
2023-03-24 19:30:27 -0700

    Add tabs similar to VS Code

--- lua/gagan/options.lua
+++ lua/gagan/options.lua
@@ -1,7 +1,15 @@
-vim.opt.number                  = true          --Enable Line Numbers
-vim.opt.relativenumber          = true          --Enable Relative line #'s
-vim.opt.tabstop                 = 4             --4 spaces for a tab (VS Code setting)
-vim.opt.expandtab               = true          --Expand tabs to spaces
-vim.opt.cursorline              = true          --Highlight current line
-vim.opt.scrolloff               = 8             --# of Lines below current line
-vim.opt.ignorecase              = true
+local set = vim.opt     --set options
+
+-- line #'s
+set.number                  = true          --Enable Line Numbers
+set.relativenumber          = true          --Enable Relative line #'s
+
+-- tabs
+set.tabstop                 = 4
+set.softtabstop             = 4
+set.shiftwidth              = 4
+set.expandtab               = true          --Expand tabs to spaces
+
+set.cursorline              = true          --Highlight current line
+set.scrolloff               = 8             --# of Lines below current line
+set.ignorecase              = true
