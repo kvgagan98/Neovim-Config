3850692fb0edd128c29a335487949a97a5390d33
kvgagan98 <kvgagan98@gmail.com>
2023-03-26 16:19:26 -0700

    Correct keymap error

--- lua/gagan/remap.lua
+++ lua/gagan/remap.lua
@@ -34,7 +34,7 @@ vim.keymap.set("n", "<leader>vat", ":Vista finder!<CR>")
 
 --Moving Text
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
 
